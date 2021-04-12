0a2c3f4c982bc3df1ed268af2f813207871f54e2
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-12 18:55:59 -0500

    add houses to house local state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { CitySvg } from './svg/CitySvg';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<CitySvg />
+		</div>
+	);
 }
 
 export default App;
