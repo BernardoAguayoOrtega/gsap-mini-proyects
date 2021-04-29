598dbc67d919058a80442ba77bdca39a421b7d8c
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-29 00:12:59 -0500

    update

--- src/html/main.js
+++ src/html/main.js
@@ -1,25 +1,12 @@
-
-gsap.registerPlugin(ScrollTrigger);
-
-const clean = () => {
-	const tl = new TimelineMax();
-	tl.set('#container', { display: 'none' }, 0)
-		.set('#green-figures > *', { autoAlpha: 0 }, 0)
-		.set('#letters > text', { autoAlpha: 0 }, 0)
-		.set('#container', { display: 'block' }, 0);
-
-	return tl;
-};
-
 const tl = new TimelineMax();
+const controller = new ScrollMagic.Controller();
 
-tl.add(clean())
-	.staggerFrom(
-		'#gray-figures > *',
-		1,
-		{ scale: 0, transformOrigin: 'center center' },
-		0.2,
-	)
+tl.staggerFrom(
+	'#gray-figures > *',
+	1,
+	{ scale: 0, transformOrigin: 'center center' },
+	0.2,
+)
 	.to('#letters > text', 1, { autoAlpha: 1 })
 	.staggerFrom(
 		'#green-figures > *',
@@ -34,18 +21,19 @@ tl.add(clean())
 		0.1,
 	);
 
-	ScrollTrigger.create({
-		trigger: "#container",
-		animation: tl,
-		// Uncomment these to see how they affect the ScrollTrigger
-		markers: true,
-		start: "top center+=300px",
-		end: "top center",
-		// toggleClass: "active",
-		// pin: true,
-		scrub: 1,
-		// onUpdate: self => {
-		//   console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
-		// }
-	});
-		
+const clean = () => {
+	const tl = new TimelineMax();
+	tl.set('#container', { display: 'none' }, 0)
+		.set('#green-figures > *', { autoAlpha: 0 }, 0)
+		.set('#letters > text', { autoAlpha: 0 }, 0)
+		.set('#container', { display: 'block' }, 0);
+};
+
+clean();
+
+var scene = new ScrollMagic.Scene({
+	triggerElement: '#stage',
+	triggerHook: 1,
+})
+	.setTween(tl)
+	.addTo(controller);
